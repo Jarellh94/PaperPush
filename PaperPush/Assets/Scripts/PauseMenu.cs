bb25cf2a15f974d2c80c01709d6b10fa03d0bd8b
Jarell Hackett <Jarellh94@gmail.com>
2018-04-02 16:02:10 -0400

    Menus Update
    Added Menus. Main menu and pause menu. Added navigation between scenes through menus.

---
+++ PaperPush/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool isPaused = false;
+    public GameObject pauseMenu;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PauseGame()
+    {
+        isPaused = true;
+
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        isPaused = false;
+
+        Time.timeScale = 1f;
+    }
+}
---
+++ PaperPush/Assets/Scripts/ToggleOff.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleOff : MonoBehaviour {
+
+    Toggle myToggle;
+
+	// Use this for initialization
+	void Start () {
+        myToggle = GetComponent<Toggle>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Off()
+    {
+        myToggle.isOn = false;
+    }
+}
