3c1b28d703f94d7f01f78806232b049574a0f798
Jarell Hackett <Jarellh94@gmail.com>
2018-04-17 13:33:28 -0400

    Added Script For Changing Settings
    Added a settings button script that the buttons will call to set the game settings. This is deone because of the static game settings object not being able to be pointed to by the buttons upon loading the scene. The settings button script finds the game settings script and passes along the settings.

---
+++ PaperPush/Assets/Scripts/SettingsButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsButton : MonoBehaviour {
+
+    GameSettings settings;
+
+	// Use this for initialization
+	void Start () {
+        settings = FindObjectOfType<GameSettings>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetGameMode(bool value)
+    {
+        settings.SetTimedGame(value);
+    }
+
+    public void SetDifficulty(int value)
+    {
+        settings.SetDifficulty(value);
+    }
+}
